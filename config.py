3e90427722a2e963baacb619d2d156a644725ff4
Deiby562 <deib2500@gmail.com>
2026-06-08 21:03:42 -0500

    pq

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+""""
+
+"""
+
+# --- Rutas ---
+MODEL_PATH = "Modelos/best1.pt"
+# Puedes añadir rutas a carpetas de log o exportación
+OUTPUT_FOLDER = "Resultados/"
+
+# --- Parámetros de Inferencia (YOLO) ---
+CONFIDENCE_THRESHOLD = 0.6  # Ajusta qué tan "seguro" debe estar el modelo
+IOU_THRESHOLD = 0.45        # Evita cajas duplicadas sobre el mismo objeto
+IMAGE_SIZE = 640            # Tamaño al que se redimensionan las imágenes
+
+# --- Configuración de Cámara ---
+CAMERA_INDEX = 0            # 0 es la cámara por defecto
+FPS_LIMIT = 30              # Controla la fluidez del video
+
+# --- Etiquetas (Opcional, ayuda a la UI) ---
+LABELS = ["Manzana", "Naranja", "Plátano"]
